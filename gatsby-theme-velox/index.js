5d2d4de51823dc5109587afa4f6569ae49228587
drmats <drmats@users.noreply.github.com>
2021-11-10 08:46:19 +0100

    testbed: referencing 'gatsby-theme-velox' plugin.

--- gatsby-theme-velox/index.js
+++ gatsby-theme-velox/index.js
@@ -1,1 +1,8 @@
+/**
+ * Gatsby-theme-velox.
+ *
+ * @license BSD-2-Clause
+ * @copyright Mat. 2021-present
+ */
+
 // no-op
---
+++ testbed/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @license BSD-2-Clause
+ * @copyright Mat. 2021-present
+ */
+
+module.exports = {
+    plugins: [
+        "gatsby-theme-velox",
+    ],
+};
